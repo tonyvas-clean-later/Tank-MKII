e5f7ce27672b62ff6060c00f4df4d25b09a035d2
Antony Vasiliev <tony.vasiliev8@gmail.com>
2022-04-28 00:23:50 -0400

    moved camera streamer into project

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const {spawn} = require('child_process');
+
 const ExpressController = require('./controllers/expressController');
 const SocketIOController = require('./controllers/socketIOController');
 
@@ -6,11 +8,31 @@ function setEnv(){
     process.env.PUBLIC_PATH = `${__dirname}/public`;
     process.env.CONFIG_FILE = `${__dirname}/config.json`
     process.env.CAMERA_URL = `http://127.0.0.1:8080?action=snapshot`
+    process.env.CAMERA_SCRIPT = `${__dirname}/mjpg-streamer/start.sh`
     process.env.MOTOR_SCRIPT = `${__dirname}/python/motor.py`;
     process.env.SERVO_SCRIPT = `${__dirname}/python/servo.py`;
     process.env.LASER_SCRIPT = `${__dirname}/python/laser.py`;
 }
 
+function startStreamer(){
+    let streamer = spawn('bash', [process.env.CAMERA_SCRIPT]);
+
+    streamer.stdout.on('data', data => {
+        console.log(data.toString());
+    })
+
+    streamer.stderr.on('data', data => {
+        console.error(data.toString());
+    })
+
+    streamer.on('exit', code => {
+        console.log(code);
+    })
+
+    return streamer;
+}
+
 setEnv();
+const streamer = startStreamer();
 const server = ExpressController.start();
 SocketIOController.start(server);
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /home/pi/Tank-MKII
-PORT=80 /usr/bin/node app.js
+PORT=80 /usr/bin/node app.js &> logs/$(date +"%s").log
